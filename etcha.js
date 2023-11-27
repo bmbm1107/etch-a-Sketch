39306c6d55725724635b9203ba0604655c1190a2
bmbm1107 <marczisbalazs@gmail.com>
2023-11-27 15:19:41 +0100

    add random color feature to newGrid(), also add randomized darkening effect

--- etcha.js
+++ etcha.js
@@ -1,7 +1,14 @@
 const container = document.querySelector('.container')
 const btnNewGrid = document.querySelector('button')
-
 btnNewGrid.addEventListener('click', newGrid)
+
+const toRGBObject = rgbStr => {
+    const [red, green, blue] = rgbStr.match(/\d+/g).map(Number);
+    return { red, green, blue };
+  };
+
+
+
 
 for (let i = 0; i < 16; i++){
 const column = document.createElement('div')
@@ -43,11 +50,32 @@ function newGrid () {
     const allBoxes = Array.from(document.querySelectorAll('.block'))
 
     for (element of allBoxes) {
-        console.log(element)
 
         element.style.width = `${boxWidth}px`;
         element.style.height = `${boxWidth}px`;
-        element.addEventListener('mouseover', (event) => event.target.style.backgroundColor = 'blue')
+    
+
+
+        element.addEventListener('mouseover', (event) => {
+           let color = event.target.style.backgroundColor
+            
+            if (color === '') {
+            let red =  Math.floor(Math.random() * 256)
+            let green =  Math.floor(Math.random() * 256)
+            let blue =  Math.floor(Math.random() * 256)
+            event.target.style.backgroundColor = `rgb(${red} ${green} ${blue})`
+
+        } else {
+            color = toRGBObject(color) 
+            newRed = Math.floor(Math.random() * (color.red - 0.5))
+            newGreen = Math.floor(Math.random() * (color.green - 0.5))
+            newBlue = Math.floor(Math.random() * (color.blue - 0.5))
+            event.target.style.backgroundColor = `rgb(${newRed} ${newGreen} ${newBlue})`
+
+        }
+      
+    
+    })
     }
 
 
